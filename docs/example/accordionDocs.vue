c3613175c0d01c37fc001d1f3f4a4a3bbbf0e2ab
okoala <dapixp@gmail.com>
2015-10-12 16:25:21 +0800

    Update

---
+++ docs/example/accordionDocs.vue
@@ -0,0 +1,124 @@
+<template>
+  <div class="bs-docs-section" id="accordion">
+    <h1 class="page-header"><a href="#accordion" class="anchor">Accordion</a></h1>
+    <div class="bs-example">
+      <p>
+        <input type="checkbox" v-model="checked">
+        Open only one at a time.
+      </p>
+      <accordion one-at-atime="{{checked}}">
+        <panel header="Panel #1" is-open="true">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+          cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+          proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </panel>
+        <panel header="Panel #2">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+          cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+          proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </panel>
+        <panel header="Panel #3">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+          cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+          proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </panel>
+        <panel header="Panel #4">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+          cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+          proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </panel>
+      </accordion>
+    </div>
+    <pre><code class="language-markup"><script type="language-mark-up">
+<input type="checkbox" v-model="checked">
+<label for="checked">Open only one at a time.</label>
+
+<accordion one-at-atime="{{checked}}">
+  <panel header="Panel #1" is-open="true">
+    ...
+  </panel>
+  <panel header="Panel #2">
+    ...
+  </panel>
+  <panel header="Panel #3">
+    ...
+  </panel>
+  <panel header="Panel #4">
+    ...
+  </panel>
+</accordion>
+</script></code></pre>
+    <h2>Accordion Options</h2>
+    <table class="table table-bordered">
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Type</th>
+          <th>Default</th>
+          <th>Description</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>one-at-time</td>
+          <td><code>Boolean</code></td>
+          <td><code>false</code></td>
+          <td>Control whether expanding an item will cause the other items to close.</td>
+        </tr>
+      </tbody>
+    </table>
+    <h2>Panel Options</h2>
+    <table class="table table-bordered">
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Type</th>
+          <th>Default</th>
+          <th>Description</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>is-open</td>
+          <td><code>Boolean</code></td>
+          <td><code>false</code></td>
+          <td>Whether accordion group is open or closed.</td>
+        </tr>
+        <tr>
+          <td>header</td>
+          <td><code>String</code></td>
+          <td></td>
+          <td>The clickable text on the group's header. You need one to be able to click on the header for toggling.</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+import accordion from 'src/Accordion.vue'
+import panel from 'src/Panel.vue'
+  export default {
+    data() {
+      return {
+        checked: true
+      }
+    },
+    components: {
+      accordion,
+      panel
+    }
+  }
+</script>
---
+++ docs/index.js
@@ -0,0 +1,96 @@
+import './assets/docs.css'
+import './assets/style.css'
+import 'prismjs'
+import './js/showLanguage'
+
+import gettingStarted from './example/gettingStarted.vue'
+import affix from 'src/Affix.vue'
+import container from './example/container.vue'
+import headerDocs from './example/headerDocs.vue'
+import alertDocs from './example/alertDocs.vue'
+import accordionDocs from './example/accordionDocs.vue'
+import affixDocs from './example/affixDocs.vue'
+import asideDocs from './example/asideDocs.vue'
+import carouselDocs from './example/carouselDocs.vue'
+import buttonsDocs  from './example/buttonsDocs.vue'
+import datepickerDocs from './example/datepickerDocs.vue'
+import dropdownDocs from './example/dropdownDocs.vue'
+import modalDocs from './example/modalDocs.vue'
+import popoverDocs from './example/popoverDocs.vue'
+import progressbarDocs from './example/progressbar-docs.vue'
+import selectDocs from './example/selectDocs.vue'
+import tabsDocs from './example/tabsDocs.vue'
+import tooltipDocs from './example/tooltipDocs.vue'
+import typeaheadDocs from './example/typeaheadDocs.vue'
+
+new Vue({
+  el: '#wrapper',
+  components: {
+    gettingStarted,
+    container,
+    affix,
+    alertDocs,
+    headerDocs,
+    accordionDocs,
+    affixDocs,
+    asideDocs,
+    carouselDocs,
+    buttonsDocs,
+    datepickerDocs,
+    dropdownDocs,
+    modalDocs,
+    popoverDocs,
+    progressbarDocs,
+    selectDocs,
+    tabsDocs,
+    tooltipDocs,
+    typeaheadDocs,
+    list: {
+      inherit: true,
+      template: `<li v-repeat="anchor"><a href="#{{$value.toLowerCase()}}">{{$value | space}}</a></li>`
+    }
+  },
+  data() {
+    return {
+      anchor: []
+    }
+  },
+  filters: {
+    space(val) {
+      return val.replace('-', ' ')
+    }
+  },
+  ready() {
+    // add h1.anchor.innerHTML to sidebar list
+    const anchor = document.querySelectorAll('.anchor')
+    this.anchor = [...anchor].map(el=> el.innerHTML.replace(' ', '-'))
+    // Scrollspy
+    const section = document.querySelectorAll('.bs-docs-section')
+    const sections = {}
+    const navbar = document.querySelector('#sidenav')
+    let i = 0
+
+    window.onload = ()=> {
+      Array.prototype.forEach.call(section, (e)=> {
+        sections[e.id] = e.offsetTop
+      })
+    }
+
+    function scrollSpy() {
+      const scrollPosition = document.documentElement.scrollTop || document.body.scrollTop
+
+      for (i in sections) {
+        // 420 = firstSection.getBoundingClientRect().top (when body.scrollTop = 0)
+        // = nav.height + header.height + firstSection.margin-top - 6 (for offset)
+        if (sections[i] + 420 <= scrollPosition) {
+          if (navbar.querySelector('.active')) navbar.querySelector('.active').className = ''
+          navbar.querySelector('a[href*=' + i + ']').parentNode.className = 'active'
+        }
+      }
+    }
+
+    window.onscroll = ()=> {
+      scrollSpy()
+    }
+  }
+})
