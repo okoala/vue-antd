78d75ce4c91cf980cfe51ffa87b0daaa2dd752c5
okoala <dapixp@gmail.com>
2015-10-12 20:09:34 +0800

    Update

---
+++ src/components/breadcrumbItem.vue
@@ -0,0 +1,20 @@
+<template>
+<span>
+  <a v-if='href' v-bind:class="prefixCls + '-link'">
+    <slot></slot>
+  </a>
+  <span v-else v-bind:class="prefixCls + '-link'">
+    <slot></slot>
+  </span>
+  <span v-bind:class="prefixCls + '-slash'">/</span>
+</span>
+</template>
+
+<script>
+export default {
+  props: {
+    prefixCls: String,
+    href: String
+  }
+}
+</script>
---
+++ src/components/carousel.vue
@@ -0,0 +1,41 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  props: {
+    dots: {
+      type: Boolean,
+      default: true
+    },
+    arrows: {
+      type: Boolean,
+      default: false
+    },
+    effect: String,
+    fade: Boolean,
+    draggable: Boolean,
+    vertical: Boolean
+  },
+
+  data () {
+    return {
+      className: 'ant-carousel'
+    }
+  },
+
+  methods: {
+    compiled () {
+      if (this.effect === 'fade') {
+        this.fade = true,
+        this.draggable = false
+      }
+
+      if (this.vertical) {
+        this.className += ' ant-carousel-vertical'
+      }
+    }
+  }
+}
+</script>
