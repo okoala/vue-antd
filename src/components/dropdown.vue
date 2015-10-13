85bd59cd17a82fd86def5304cf66b27c01bead5a
okoala <dapixp@gmail.com>
2015-10-13 18:13:36 +0800

    Update

---
+++ src/components/dropdown.vue
@@ -0,0 +1,61 @@
+<template>
+<Tooltip
+  ref="tooltip"
+  :visible.sync="visible"
+  :afterVisibleChange="_afterVisibleChange"
+  :onVisibleChange="_onVisibleChange">
+  <slot name='overlay' :class="prefixCls + '-menu'" @click="_onClick"></slot>
+  <slot></slot>
+</Tooltip>
+</template>
+
+<script>
+import Tooltip from './base/tooltip'
+import { defaultProps } from '../utils'
+
+export default {
+  props: defaultProps({
+    transitionName: 'slide-up',
+    prefixCls: 'ant-dropdown',
+    minOverlayWidthMatchTrigger: true,
+    visible: Boolean,
+    defaultVisible: false,
+    onVisibleChange: () => {}
+  }),
+
+  components: { Tooltip }
+
+  data () {
+    return {
+      placement: {
+        points: ['tl', 'bl']
+      }
+    }
+  },
+
+  methods: {
+    compiled () {
+      if (this.visible == null) {
+        this.visible = this.defaultVisible
+      }
+    },
+
+    _onClick (e) {
+
+    },
+
+    _onVisibleChange (value) {
+      this.visible = value
+      this.onVisibleChange(value)
+    },
+
+    // todo
+    _afterVisibleChange (visible) {
+      if (visible && this.minOverlayWidthMatchTrigger) {
+        const overlayNode = this.$el
+      }
+    },
+  }
+}
+
+</script>
