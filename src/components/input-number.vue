8b78152e76e0c49bfdf5c02e5fe2b6321a37860a
okoala <dapixp@gmail.com>
2015-10-14 18:09:05 +0800

    Update

---
+++ src/components/input-number.vue
@@ -0,0 +1,144 @@
+<template>
+<div :class="classes" :style="style">
+  <div :class="prefixCls + '-handler-wrap'">
+    <a unselectable="unselectable"
+       ref="up"
+       @click="upDisabledClass ? noop : up"
+       @mouse.down="preventDefault"
+       :class="prefixCls + '-handler ' + prefixCls + '-handler-up ' + upDisabledClass">
+      <span unselectable="unselectable"
+            :class="prefixCls + '-handler-up-inner'"
+            @click="preventDefault"/>
+    </a>
+    <a unselectable="unselectable"
+       ref="down"
+       @mouse.down="preventDefault"
+       @click="downDisabledClass ? noop : down"
+       :class="prefixCls + '-handler ' + prefixCls + '-handler-down ' + downDisabledClass">
+      <span unselectable="unselectable"
+            :class="prefixCls + '-handler-down-inner'"
+            @click="preventDefault"/>
+    </a>
+  </div>
+  <div :class="prefixCls + '-input-wrap'">
+    <input ref="input"
+           autoComplete="off"
+           @focus="_onFocus"
+           @blur="_onBlur"
+           @keydown.stop="_onKeyDown"
+           @change="_onChange"
+           :class="prefixCls + '-input'"
+           :autoFocus="autoFocus"
+           :readOnly="readOnly"
+           :disabled="disabled"
+           :max="max"
+           :min="min"
+           :name="name"
+           :value="value"/>
+  </div>
+</div>
+</template>
+
+<script>
+import { defaultProps } from '../utils'
+import { classSet } from 'rc-util'
+
+function noop () {}
+
+function isValueNumber (value) {
+  return (/^-?\d+?$/).test(value + '')
+}
+
+function preventDefault (e) {
+  e.preventDefault()
+}
+
+export default {
+  props: defaultProps({
+    prefixCls: 'ant-input-number',
+    max: Infinity,
+    min: -Infinity,
+    style: {},
+    size: String,
+    value: String,
+    defaultValue: '',
+    autoFocus: false,
+    onChange: noop,
+    readOnly: false,
+    disabled: false
+  }),
+
+  data () {
+    return {
+      sizeClass: '',
+      noop: noop,
+      preventDefault: preventDefault
+    }
+  }
+
+  methods: {
+    compiled () {
+      if (this.size === 'large') {
+        this.sizeClass = 'ant-input-number-lg'
+      } else if (this.size === 'small') {
+        this.sizeClass = 'ant-input-number-sm'
+      }
+
+      if (this.value == null) {
+        this.value = this.defaultValue
+      }
+
+      this.focused = this.autoFocus
+    },
+
+    _setValue (value, callback) {
+      this.value = value
+      this.onChange(value)
+      callback()
+    },
+
+    _onChange (event) {
+      let val = event.target.value.trim()
+
+      if (!val) {
+        this._setValue(val)
+      } else if (isValueNumber(val)) {
+        val = Number(val)
+
+        if (val < this.min) return
+        if (val > this.max) return
+
+        this._setValue(val)
+      } else if (val === '-') {
+        if (this.min >= 0) return
+        this.value = val
+      }
+    },
+
+    _onKeyDown (e) {
+      if (e.keyCode === 38) {
+        this._up(e)
+      } else if (e.keyCode === 40) {
+        this._down(e)
+      }
+    },
+
+    _onFocus () {
+      this.focused = true
+    },
+
+    _onBlur () {
+      this.focused = false
+
+      if (this.value === '-') {
+        this._setValue('')
+      }
+    },
+
+    _step (type, e) {
+
+    }
+  }
+}
+
+</script>
