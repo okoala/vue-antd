e31c37363321bf7e77cbdfea7222f4adad032a92
okoala <dapixp@gmail.com>
2015-10-22 11:47:39 +0800

    添加image loader

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -52,6 +52,13 @@ module.exports = {
         })
       },
       {
+        test: /.*\.(gif|png|jpe?g|svg)$/i,
+        loaders: [
+          'file?hash=sha512&digest=hex&name=[hash].[ext]',
+          'image-webpack?{progressive:true, optimizationLevel: 7, interlaced: false, pngquant:{quality: "65-90", speed: 4}}'
+        ]
+      },
+      {
         test: /\.js$/,
         exclude: /(node_modules|bower_components)/,
         loader: 'babel!eslint'
--- docs/views/docs/introduce.vue
+++ docs/views/docs/introduce.vue
@@ -1,14 +1,14 @@
 <template lang="md">
-# Ant Design of React
+# Ant Design of Vue
 
 ---
 
-这里是 Ant Design 的 React 实现，开发和服务于企业级后台产品。
+这里是 Ant Design 的 Vue 实现，开发和服务于企业级后台产品。
 
 <div class="pic-plus">
   <img width="150" src="https://t.alipayobjects.com/images/rmsweb/T11aVgXc4eXXXXXXXX.svg">
   <span>+</span>
-  <img width="160" src="https://t.alipayobjects.com/images/rmsweb/T16xRhXkxbXXXXXXXX.svg">
+  <img width="150" src="../../styles/images/vue.png">
 </div>
 <style>
 .pic-plus > * {
@@ -26,39 +26,27 @@
 ## 特性
 
 - Designed as Ant Design.
-- [React Component](http://react-component.github.io/badgeboard/) 上二次封装的丰富实用的 UI 组件。
-- 基于 React 的组件化开发模式。
+- 基于 Vue 的组件化开发模式。
 - 背靠 npm 生态圈。
 - 基于 webpack 的调试构建方案，支持 ES6。
 
 ## 示例
 
-```jsx
-import { Datepicker } from 'antd';
-React.render(<Datepicker />, mountNode);
-```
 
 ## 版本
 
-- 稳定版：[![npm package](http://img.shields.io/npm/v/antd.svg?style=flat-square)](https://www.npmjs.org/package/antd)
-- 开发版：[![](https://cnpmjs.org/badge/v/antd.svg?&tag=beta&subject=npm)](https://www.npmjs.org/package/antd)
 
 ## 链接
 
-- [首页](http://ant.design/)
-- [文档](http://ant.design/docs/introduce)
-- [组件](http://ant.design/components/)
-- [构建调试 antd-bin](https://github.com/ant-design/antd-bin)
-- [开发计划](https://github.com/ant-design/ant-design/issues/9)
-- [React 模块](http://react-component.github.io/)
-- [React 代码规范](https://github.com/react-component/react-component.github.io/blob/master/docs/zh-cn/component-code-style.md)
-- [组件设计原则](https://github.com/react-component/react-component.github.io/blob/master/docs/zh-cn/component-design.md)
+- [首页](/vue-antd/)
+- [文档](/introduce)
+- [组件](/components/)
 
 ## 谁在使用
 
-- 蚂蚁金服
+- 本人
 
 ## 如何贡献
 
-我们欢迎任何形式的贡献，有任何建议或意见您可以进行 [Pull Request](https://github.com/ant-design/ant-design/pulls)，或者给我们[提问](https://github.com/ant-design/ant-design/issues)。
+我们欢迎任何形式的贡献，有任何建议或意见您可以进行 [Pull Request](https://github.com/okoala/vue-antd/pulls)，或者给我们[提问](https://github.com/ant-design/ant-design/issues)。
 </template>
