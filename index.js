32209b79711c16f86dbf5ffc2059ea2d6358f2fd
okoala <dapixp@gmail.com>
2015-11-02 11:01:01 +0800

    暴露目前已经完成的组件到index.js

--- index.js
+++ index.js
@@ -10,10 +10,49 @@ window.matchMedia = window.matchMedia || function () {
   }
 }
 
-import radio from './components/radio'
+import vTag from './components/tag'
+import vSpin from './components/spin'
+import vBadge from './components/badge'
+import vTable from './components/table'
+import vAffix from './components/affix'
+import vIcon from './components/iconfont'
+import vSwitch from './components/switch'
+import vUpload from './components/upload'
+import vCheckbox from './components/checkbox'
+import { vRow, vCol } from './components/layout'
+import vSteps, { vStep } from './components/steps'
+import vInputNumber from './components/input-number',
+import vSelect, { vOption } from './components/select'
+import vRadio, { vRadioGroup } from './components/radio'
+import vButton, { vButtonGroup } from './components/button'
+import vBreadcrumb, { vBreadcrumbItem } from './components/breadcrumb'
+import { vProgressLine, vProgressCircle } from './components/progress'
 
 const antd = {
-  radio
+  vRow,
+  vCol,
+  vButton,
+  vButtonGroup,
+  vIcon,
+  vCheckbox,
+  vInputNumber,
+  vRadio,
+  vRadioGroup,
+  vSelect,
+  vOption,
+  vSwitch,
+  vUpload,
+  vBadge,
+  vProgressLine,
+  vProgressCircle,
+  vTable,
+  vTag,
+  vSpin,
+  vBreadcrumb,
+  vBreadcrumbItem,
+  vSteps,
+  vStep,
+  vAffix
 }
 
 antd.utils = utils
