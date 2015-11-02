6501e02bb5c2ec767050bae2adb3f027ec1eb153
okoala <dapixp@gmail.com>
2015-11-02 22:30:31 +0800

    重新整理了下构建目录~~

--- build/webpack/webpack.config.js
+++ build/webpack/webpack.config.js
@@ -53,7 +53,7 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /(node_modules|bower_components)/,
-        loader: 'babel!eslint'
+        loader: 'babel?optional[]=runtime&stage=2!eslint'
       },
       {
         test: /\.less$/,
@@ -71,7 +71,7 @@ module.exports = {
       // apply babel transform to all javascript
       // inside *.vue files.
       md: 'html!vue-antd-docs',
-      js: 'babel!eslint',
+      js: 'babel?optional[]=runtime&stage=2!eslint',
       less: ExtractTextPlugin.extract(
         'css?sourceMap&-minimize!' + 'autoprefixer-loader!' + 'less?sourceMap'
       )
