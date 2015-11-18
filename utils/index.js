7a6b8789fd2a32a205b65f54172e86512700919b
okoala <dapixp@gmail.com>
2015-11-19 00:17:10 +0800

    更新defaultProps

--- utils/index.js
+++ utils/index.js
@@ -55,6 +55,7 @@ export function defaultProps (props) {
     if (props.hasOwnProperty(i)) {
       let defaultValue = props[i]
 
+      // 支持String， Number等类型
       if (defaultValue.name && window[defaultValue.name] === defaultValue) {
         props[i] = {
           type: defaultValue,
@@ -64,9 +65,29 @@ export function defaultProps (props) {
         continue
       }
 
-      if (isPlainObject(defaultValue)) continue
-
       let type = toString.call(defaultValue).replace('[object ', '').replace(']', '')
+
+      // 如果传进来的是vue的原生props对象的话
+      if (type === 'Object') {
+        if (defaultValue.type != null ||
+            defaultValue.default != null ||
+            defaultValue.validator != null ||
+            defaultValue.twoWay != null ||
+            defaultValue.required != null) {
+          continue
+        }
+      }
+
+      // 支持 Object和Array的简洁声明方式
+      // Todo: 目前看来这样并没有什么卵用
+      if (type === 'Array' || type === 'Object') {
+        props[i] = {
+          type: window[type],
+          default: function () {
+            return defaultValue
+          }
+        }
+      }
 
       props[i] = {
         type: window[type],
