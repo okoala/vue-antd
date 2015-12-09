8089123a14ecb0e745d644617b0749bef71f7bc5
okoala <dapixp@gmail.com>
2015-12-09 23:34:48 +0800

    Update Menu

--- components/menu/Menu.vue
+++ components/menu/Menu.vue
@@ -1,7 +1,57 @@
 <template>
-
+<menu
+  :theme="theme"
+  :mode="mode"
+  :selected-keys="selectedKeys"
+  :default-selected-keys="defaultSelectedKeys"
+  :open-keys="openKeys"
+  :default-open-keys="defaultOpenKeys"
+  :style="style"
+  :on-click="_handleClick"
+  :on-open="_handleOpenKeys"
+  :on-close="_handleCloseKeys"
+  :on-select="onSelect"
+  :on-deselect="onDeselect"
+  :class-name="className + ' ' + prefixCls + '-' + theme">
+  <slot></slot>
+</menu>
 </template>
 
 <script>
+import { defaultProps, oneOf } from '../../utils'
+import Menu from '../base/menu'
 
+export default {
+  props: defaultProps({
+    theme: oneOf(['light', 'dark'], 'light'),
+    mode: oneOf(['vertical', 'horizontal', 'inline'], 'vertical'),
+    selectedKeys: Array,
+    defaultSelectedKeys: Array,
+    openKeys: Array,
+    defaultOpenKeys: Array,
+    style: Object,
+    onClick () {},
+    onSelect () {},
+    onDeselect () {}
+  }),
+
+  components: { Menu },
+
+  methods: {
+    _handleClick (e) {
+      this.openKeys = []
+      this.onClick()
+    },
+
+    _handleOpenKeys (e) {
+      this.openKeys = e.openKeys
+      this.onOpen()
+    },
+
+    _handleCloseKeys (e) {
+      this.openKeys = e.openKeys
+      this.onClose()
+    }
+  }
+}
 </script>
